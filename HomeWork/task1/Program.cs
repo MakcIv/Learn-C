bcc4af485d801a14ab4ee2b7b92ec8865d7e278c
Makc <killsam@mail.ru>
2022-10-03 20:32:52 +0300

    task1

--- HomeWork/task1/Program.cs
+++ HomeWork/task1/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    System.Console.WriteLine("больше {a}");
+}
+else if (a<b)
+{
+    System.Console.WriteLine("больше {b}");
+}
