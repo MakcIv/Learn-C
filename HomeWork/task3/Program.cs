181bf07c824d44f9df7c428545fd78e0de963f5e
Makc <killsam@mail.ru>
2022-10-03 22:30:47 +0300

    рабочее решение task3

--- HomeWork/task3/Program.cs
+++ HomeWork/task3/Program.cs
@@ -2,9 +2,9 @@
 int num = Convert.ToInt32(Console.ReadLine());
 if (num % 2 == 1)
 {
-    Console.WriteLine("число нечетное", num, "нечетное");
+    Console.WriteLine("число нечетное", num,"");
 }
 else
 {
-    Console.WriteLine("число четное", num, "четное");
+    Console.WriteLine("число четное", num,"");
 }
