279e1df19c5126f0a13fa26353422d8d41e6bc90
Makc <killsam@mail.ru>
2022-10-03 22:09:19 +0300

    решение task3

---
+++ HomeWork/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 1)
+{
+    Console.WriteLine("число нечетное", num, "нечетное");
+}
+else
+{
+    Console.WriteLine("число четное", num, "четное");
+}
