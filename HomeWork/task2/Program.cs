661e99ba205b4c00cbdf459ecabb8fcf80db4bdb
Makc <killsam@mail.ru>
2022-10-03 21:28:48 +0300

    решение task2

--- HomeWork/task2/Program.cs
+++ HomeWork/task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("введите три числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b||a>c)
+{
+    System.Console.WriteLine("больше {a}");
+}
+else if (a<b||b>c)
+{
+    System.Console.WriteLine("больше {b}");
+}
+else if (b<c||c>a)
+{
+    System.Console.WriteLine("больше {c}");
+}
