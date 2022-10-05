74932c7e6026fd79af91944a74c156e8a821e3ee
Makc <killsam@mail.ru>
2022-10-05 07:37:09 +0300

    task4

--- HomeWork/task4/Program.cs
+++ HomeWork/task4/Program.cs
@@ -1,2 +1,6 @@
-﻿System.Console.WriteLine("Введите любое число");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Введите число:");
+num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+    if (i % 2= 1)
